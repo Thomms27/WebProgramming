9a24bc69307b7642df4580790716837c743bc9f8
Thomas <thomms27@hotmail.com>
2015-12-03 13:08:13 +1100

    Contact.php
    Just a contact us page. This can be done way better than have hard coded text, but at the time we were given a copy/paste.

---
+++ Contact.php
@@ -0,0 +1,87 @@
+<?php
+	include('brain.php');
+if(isset($_SESSION['email'])){
+	echo "<h3>
+	<form action='' method='post'>
+		<input type='submit' name='ViewOrder' value='ViewOrder' />
+	</form>
+	</h3>";
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>	Golden spacetour</title>
+		<style>
+
+		</style>
+		<link rel= "stylesheet" type="text/css" href="styleSheet.css" />
+	</head>
+
+	<body>
+			
+			<div>
+				<p> <b>This following document sets forth the Privacy Policy for the Golden space tour website.</b> <br />
+
+				Golden Space tour is committed to providing you with the best possible customer service experience. Golden Space tour is bound by
+				the Privacy Act 1988 (Crh), which sets out a number of principles concerning the privacy of individuals. <br />
+
+				<b>Collection of your personal information</b> <br /><br />
+
+				There are many aspects of the site which can be viewed without providing personal information, however, 
+				for access to future Golden Space tour customer support features you are required to submit personally identifiable information. 
+				This may include but not limited to a unique username and password, or provide sensitive information in the recovery of your lost password. <br />
+
+				<br /><b>Sharing of your personal information </b> <br />
+				We may occasionally hire other companies to provide services on our behalf, including but not limited to handling customer support enquiries, 
+				processing transactions or customer freight shipping. Those companies will be permitted to obtain only the personal information they need to
+				deliver the service. Golden Space tour takes reasonable steps to ensure that these organisations are bound by confidentiality and privacy 
+				obligations in relation to the protection of your personal information. <br /><br />
+				<b>Use of your personal information </b> <br /><br />
+				For each visitor to reach the site, we expressively collect the following non-personally identifiable information, 
+				including but not limited to browser type, version and language, operating system, pages viewed while browsing the Site, 
+				page access times and referring website address. This collected information is used solely internally for the purpose of gauging visitor traffic,
+				trends and delivering personalized content to you while you are at this Site.
+				From time to time, we may use customer information for new, unanticipated uses not previously disclosed in our privacy notice.
+				If our information practices change at some time in the future we will use for these new purposes only, 
+				data collected from the time of the policy change forward will adhere to our updated practices. <br /> <br />
+				<b>Changes to this Privacy Policy </b> <br /> <br />
+
+				Golden Space tour reserves the right to make amendments to this Privacy Policy at any time. If you have objections to the Privacy Policy,
+				you should not access or use the Site.<br /><br />
+				<b>Accessing Your Personal Information</b><br /><br />
+				You have a right to access your personal information, subject to exceptions allowed by law. If you would like to do so, please let us know.
+				You may be required to put your request in writing for security reasons. Golden Space tour reserves the right to charge a fee for searching for,
+				and providing access to, your information on a per request basis.
+			 
+				Contacting us
+				Golden Space tour welcomes your comments regarding this Privacy Policy. 
+				If you have any questions about this Privacy Policy and would like further information, 
+				please contact us by any of the following means during business hours Monday to Friday.
+				<br /><br /><br /><br /><br />
+				Call: (03) 0000 0000<br />
+				Post: <br />
+				Golden Space tour,<br />
+				123 helloworld Avenue,<br />
+				helloworld Street, VIC 1234,<br />
+				AUSTRALIA <br />
+				E-mail: privacy-policy@ Golden_Space_tour.com<br />
+	   
+				
+				<br />
+			</div>
+			<hr/>
+			<div id="footer">
+				<a href="http://validator.w3.org/check?uri=referer">HTML Validator</a>
+				<a href="http://jigsaw.w3.org/css-validator/check/referer">CSS Validator</a>
+			</div>
+		<?php
+			include("footer.php");
+		?>
+		</div>
+
+	</body>
+
+</html>
