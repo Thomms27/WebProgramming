a0570218ee24931d741205588d5717f3b08e8cf2
Thomas <thomms27@hotmail.com>
2015-12-03 13:10:17 +1100

    footer.php
    The footer file which is included in most of the php files.

---
+++ footer.php
@@ -0,0 +1,20 @@
+<div id="debug" style="border: 1px solid white; overflow: auto; color: #FFFFFF;">
+
+<pre>
+$_SESSION <?php print_r ($_SESSION); ?>
+
+$_POST <?php print_r ($_POST); ?>
+
+$_GET <?php print_r ($_GET); ?>
+
+$_COOKIE <?php print_r ($_COOKIE); ?>
+</pre>
+</div>
+
+<pre>
+<?php
+    // Might also be worth learning about:
+    //var_dump($_SESSION);
+    //var_export($_SESSION);
+?>
+</pre>
